838954fcb5725b83d16c433d6c4996a08dd0b79e
Caesar-PT <73921520+Caesar-PT@users.noreply.github.com>
2020-12-13 22:55:56 +0700

    f

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 8, 9};
+        int[] arr2 = {4, 5, 6, 7};
+        int[] arr3 = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++){
+            arr3[i] = arr1[i];
+        }
+        for (int j = 0; j < arr2.length; j++) {
+            arr3[arr1.length + j] = arr2[j];
+        }
+
+        System.out.println(Arrays.toString(arr3));
+
+    }
+}
